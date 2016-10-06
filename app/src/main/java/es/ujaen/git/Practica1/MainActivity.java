32176679900a6c2a91705f69389578c7f0c5d04b
FrancescoJose <fjam0007@estudiante.ujaen.es>
2016-10-06 22:15:12 +0200

    Autenticacion y cambio entre activitys

--- app/src/main/java/es/ujaen/git/Practica1/MainActivity.java
+++ app/src/main/java/es/ujaen/git/Practica1/MainActivity.java
@@ -1,7 +1,13 @@
 package es.ujaen.git.Practica1;
 
+import android.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        AuthFragment au = AuthFragment.newInstance("Juan","12345");
+        ft.add(R.id.main_frame,au);
+        ft.addToBackStack(null);
+        ft.commit();
+
+
+
     }
+
+    private String user;
+    private String pass;
+
+    public void Autenticacion(View view){
+
+        final EditText txtNombre = (EditText)findViewById(R.id.user2);
+
+        Intent i = new Intent(this, Autenticado.class);
+        i.putExtra("Nombre", txtNombre.getText().toString());
+        i.setClass(MainActivity.this,Autenticado.class);
+        startActivity(i);
+
+
+
+    }
+
+
+
+
+
+
 }
